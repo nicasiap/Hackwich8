9b561e3a2ceee4a70f7640dedd2448854e24881e
CM Student <cmstudent@UHWO-ACM-C-03.local>
2018-04-02 10:37:15 -1000

    Finished Hackwich8

---
+++ Hackwich8/Restaurant.swift
@@ -0,0 +1,33 @@
+//
+//  Restaurant.swift
+//  Hackwich8
+//
+//  Created by CM Student on 4/2/18.
+//  Copyright © 2018 CM Student. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Restaurant: NSObject, MKAnnotation {
+    
+   // create 3 variables hold data for the annotation
+    let restaurantTitle: String?
+    let restaurantType: String
+    let coordinate: CLLocationCoordinate2D
+    
+    //initialize the MKAnnotation object
+    
+    init(title: String, type: String, coordinate: CLLocationCoordinate2D) {
+        self.restaurantTitle = title
+        self.restaurantType = type
+        self.coordinate = coordinate
+        
+        super.init()
+     }
+    
+    var subtitle: String? {
+        return restaurantType
+    }
+
+}
--- Hackwich8/ViewController.swift
+++ Hackwich8/ViewController.swift
@@ -7,14 +7,51 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    //set initial location to UHWO
+    let initialLocation = CLLocation(latitude: 21.361888 , longitude: -158.055725)
+    //set region around UHWO to 1000m
+    //everything is done with meters
+    let regionRadius: CLLocationDistance = 10000
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let restaurantOne = Restaurant(title: "Kalapawai", type: "Kalapawai", coordinate: CLLocationCoordinate2D(latitude: 21.346470, longitude: -158.080098))
+        mapView.addAnnotation(restaurantOne)
+        
+        centerMapLocation(location: initialLocation)
+        
     }
+    
+    
+    
+    
+    
+    
+    func centerMapLocation (location: CLLocation){
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    
+    
+    
 
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
